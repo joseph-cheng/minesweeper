cf124405d39da90865eb9aa41ab5c93fc9bbe64b
Joe <joe.cheng2001@gmail.com>
2019-02-01 19:51:12 +0000

    added spreading visibility

--- state.py
+++ state.py
@@ -7,7 +7,8 @@ class State:
         self.w = w
         self.h = h
         self.generate_board()
-
+        
+        self.visible_board = [[False for col in range(self.w)] for row in range(self.h)]
 
     def generate_board(self):
         self.board = [[0 for col in range(self.w)] for row in range(self.h)]
@@ -25,8 +26,51 @@ class State:
                 if self.board[row][col] != -1:
                     self.board[row][col] = self.count_mines(row,col)
         
+    def no_mine(self, row, col):
+        cell = self.board[row][col]
+        if cell == -1:
+            print("UH OH!")
+            return
 
-                
+        self.spread_visibility(row,col)
+
+    def spread_visibility(self, row, col):
+        self.visible_board[row][col] = True
+        if self.board[row][col] == 0:
+
+            #Check left hand side
+            if row > 0:
+                if self.visible_board[row-1][col] == False:
+                    self.spread_visibility(row-1, col)
+                if col > 0:
+                    if self.visible_board[row-1][col-1] == False:
+                        self.spread_visibility(row-1, col-1)
+                if col < self.h-1:
+                    if self.visible_board[row-1][col+1] == False:
+                        self.spread_visibility(row-1, col+1)
+
+            #Check right hand side
+            if row < self.w-1:
+                if self.visible_board[row+1][col] == False:
+                        self.spread_visibility(row+1, col)
+                if col > 0:
+                    if self.visible_board[row+1][col-1] == False:
+                        self.spread_visibility(row+1, col)
+                if col < self.h-1:
+                    if self.visible_board[row+1][col+1] == False:
+                        self.spread_visibility(row+1, col+1)
+
+            #check top
+            if col > 0:
+                if self.visible_board[row][col-1] == False:
+                        self.spread_visibility(row, col-1)
+
+            #check bottom
+            if col < self.h -1:
+                if self.visible_board[row][col+1] == False:
+                        self.spread_visibility(row, col+1)
+        
+        
     def count_mines(self, row, col):
         neighbouring_mines = 0
         
